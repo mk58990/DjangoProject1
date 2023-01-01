2a04765fe5872b6f8bcfb9888b2d115fa6ec61ed
mk58990 <mk58990@gmail.com>
2023-01-01 12:24:00 +0530

    space added between the functions

--- IEapp/views.py
+++ IEapp/views.py
@@ -12,22 +12,26 @@ class FoodFilter(APIView):
         fooddetails=Foodsales.objects.filter(Product__iexact=Product)
         serobj=FoodsalesSr(fooddetails,many=True)
         return Response(serobj.data)
+
     def post(self,r,Product):
         fooddetails=Foodsales.objects.filter(Product__iexact=Product)[0:5]
         serobj=FoodsalesSr(fooddetails,many=True)
         return Response(serobj.data)
+
 
 class FoodUpdateDelete(APIView):
     def get(self,r):
         fooddetails=Foodsales.objects.all()
         serobj = FoodsalesSr(fooddetails, many=True)
         return Response(serobj.data)
+
     def post(self,r):
         serobj=FoodsalesSr(data=r.data)
         if serobj.is_valid():
             serobj.save()
             return Response(serobj.data,status=status.HTTP_201_CREATED)
         return Response(serobj.errors,status=status.HTTP_400_BAD_REQUEST)
+
     def put(self,r,pk):
         obj=Foodsales.objects.get(pk=pk)
         serobj=FoodsalesSr(obj,data=r.data)
@@ -35,6 +39,7 @@ class FoodUpdateDelete(APIView):
             serobj.save()
             return Response(serobj.data,status=status.HTTP_201_CREATED)
         return Response(serobj.errors,status=status.HTTP_400_BAD_REQUEST)
+    
     def delete(self,r,pk):
         obj = Foodsales.objects.get(pk=pk)
         obj.delete()
